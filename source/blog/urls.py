aac0713550ad5b65548b3ad2c7d32a52a2bfb19d
Malik-94 <imenov-a@mail.ru>
2019-10-23 00:43:09 +0600

    57 Разделил urls.py между приложениями

--- source/blog/urls.py
+++ source/blog/urls.py
@@ -14,31 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from webapp.views import IndexView, TaskCreateView, TaskUpdateView, TaskDeleteView, TaskView, StatusCreateView, \
-    StatusUpdateView, StatusList, StatusDeleteView, TypeCreateView, TypeList, TypeUpdateView, TypeDeleteView, \
-    ProjectListView
-
-from accounts.views import login_view, logout_view
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
-    path('article/<int:pk>/', TaskView.as_view(), name='task_view'),
-    path('articles/add/', TaskCreateView.as_view(), name='task_add'),
-    path('article/<int:pk>/edit/', TaskUpdateView.as_view(), name='task_update'),
-    path('article/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
-    path('status/', StatusList.as_view(), name='status_list'),
-    path('statuses/add/', StatusCreateView.as_view(), name='status_add'),
-    path('status/<int:pk>/edit/', StatusUpdateView.as_view(), name='status_update'),
-    path('status/<int:pk>/delete/', StatusDeleteView.as_view(), name='status_delete'),
-    path('type/', TypeList.as_view(), name='type_list'),
-    path('types/add/', TypeCreateView.as_view(), name='type_add'),
-    path('type/<int:pk>/edit/', TypeUpdateView.as_view(), name='type_update'),
-    path('type/<int:pk>/delete/', TypeDeleteView.as_view(), name='type_delete'),
-    path('projects/', ProjectListView.as_view(), name='project_list'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('accounts/', include('accounts.urls')),
+    path('', include('webapp.urls')),
 
 ]
 
---
+++ source/webapp/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from webapp.views import IndexView, TaskCreateView, TaskUpdateView, TaskDeleteView, TaskView, StatusCreateView, \
+    StatusUpdateView, StatusList, StatusDeleteView, TypeCreateView, TypeList, TypeUpdateView, TypeDeleteView, \
+    ProjectListView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('article/<int:pk>/', TaskView.as_view(), name='task_view'),
+    path('articles/add/', TaskCreateView.as_view(), name='task_add'),
+    path('article/<int:pk>/edit/', TaskUpdateView.as_view(), name='task_update'),
+    path('article/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
+    path('status/', StatusList.as_view(), name='status_list'),
+    path('statuses/add/', StatusCreateView.as_view(), name='status_add'),
+    path('status/<int:pk>/edit/', StatusUpdateView.as_view(), name='status_update'),
+    path('status/<int:pk>/delete/', StatusDeleteView.as_view(), name='status_delete'),
+    path('type/', TypeList.as_view(), name='type_list'),
+    path('types/add/', TypeCreateView.as_view(), name='type_add'),
+    path('type/<int:pk>/edit/', TypeUpdateView.as_view(), name='type_update'),
+    path('type/<int:pk>/delete/', TypeDeleteView.as_view(), name='type_delete'),
+    path('projects/', ProjectListView.as_view(), name='project_list'),
+
+
+]
