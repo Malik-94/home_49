4965414f1dd8bdca6872e6ab16992adfc7412bdb
Malik-94 <imenov-a@mail.ru>
2019-10-29 19:45:35 +0600

    59 home Добавил личный кабинет пользователя в приложение "Трекер задач".

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from accounts.views import login_view, logout_view, register_view
-
+from accounts.views import register_view, UserDetailView
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', register_view, name='register'),
+    path('profile/<pk>/', UserDetailView.as_view(), name='user_detail')
 ]
 
 app_name = 'accounts'
---
+++ source/accounts/views.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from accounts.forms import UserCreationForm
+from django.views.generic import UpdateView, DetailView
+
+
+def login_view(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('webapp:index')
+        else:
+            context['has_error'] = True
+    return render(request, 'registration/login.html', context=context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('webapp:index')
+
+
+def register_view(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form' : form})
+    elif request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                is_active=True)
+
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('webapp:index')
+        else:
+            return render(request,'register.html', {'form' : form})
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user_deteil.html'
+    context_object_name = 'user_obj'
