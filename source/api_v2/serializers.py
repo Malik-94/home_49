b45119d66b0feec0d63e71cb21741f28a035017c
Malik-94 <imenov-a@mail.ru>
2019-12-05 18:39:12 +0600

    home 69 При выводе проектов сделал вложенный вывод всех задач в проекте.

--- source/api_v2/serializers.py
+++ source/api_v2/serializers.py
@@ -2,15 +2,17 @@ from rest_framework import serializers
 from webapp.models import Project,Task
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Project
-        fields = ('id', 'summary', 'description', 'created_at','update_at')
-
-
 class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
         fields = ('id', 'summary', 'description', 'status', 'type', 'project',  'created_at', 'update_at')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ('id', 'summary', 'description', 'tasks', 'created_at', 'update_at')
+
